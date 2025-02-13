fc446228263190e1598c51853e389e166d9b63d3
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-02-13 18:52:54 -0300

    .
    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
+const imagemin = require ('gulp-imagemin')
 const sass = require('gulp-sass') (require('sass'));
-
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -8,10 +8,16 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
 function watchObserver(cb) {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
 
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images)
 exports.watch = watchObserver
