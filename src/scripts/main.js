8a8f23b98b7ca8aaad3063d1f98a66b45b46bc1b
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-02-25 15:36:59 -0300

    .
    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
@@ -11,7 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', switchAnswer);
+    }
 })
+
+function switchAnswer(element) {
+    const classSwitchAccordeon = 'faq__questions__item--is-open';
+    const elementFather = element.target.parentNode;
+
+    console.log(element);
+    elementFather.classList.toggle(classSwitchAccordeon)
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
