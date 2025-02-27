5a761a7e50fcbb3ab36ec365f6a0aad3ee843696
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-02-26 21:18:03 -0300

    config build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
-const imagemin = require ('gulp-imagemin')
 const sass = require('gulp-sass') (require('sass'));
+const imagemin = require ('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts () {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -15,9 +22,10 @@ function images() {
 }
 
 function watchObserver(cb) {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*js', gulp.parallel(scripts))
 }
 
 
-exports.default = gulp.parallel(styles, images)
+exports.default = gulp.parallel(styles, images, scripts)
 exports.watch = watchObserver
