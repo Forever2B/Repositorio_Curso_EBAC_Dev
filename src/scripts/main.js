425a1d05530c8f45cd6e6a2474a9f7765dc36a07
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-03-10 12:39:28 -0300

    .
    .

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,7 +11,10 @@
 //     })
 // })
 
+
 $(document).ready(function() {
+    $('#cep').mask('00000-000');
+
     $('#btn-searchcep').click(function() {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
@@ -19,20 +22,45 @@ $(document).ready(function() {
         $(this).find('i').addClass('d-none');
         $(this).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function(answer) {
-            console.log(answer);
+        // ---
 
-            const logradouro = answer.logradouro;
-            const bairro = answer.bairro;
-            const cidade = answer.localidade;
-            const estado = answer.uf;
+        fetch(endpoint).then(function(resposta){
+            return resposta.json()
+        })
+        .then(function(json){
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
             const address = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
             $('#address').val(address);
-
         })
-        setTimeout(()=>{
-            $(this).find('span').addClass('d-none');
-            $(this).find('i').removeClass('d-none');
-        }, 1000)
+        .catch(function(erro) {
+            alert("ocorreu um erro ao buscar o cep!");
+        })
+        .finally(function() {
+            setTimeout(()=>{
+                $('#btn-searchcep').find('span').addClass('d-none');
+                $('#btn-searchcep').find('i').removeClass('d-none');
+            }, 300)
+        })
+
+        // --- AJAX -
+
+        // $.ajax(endpoint).done(function(answer) {
+        //     console.log(answer);
+
+        //     const logradouro = answer.logradouro;
+        //     const bairro = answer.bairro;
+        //     const cidade = answer.localidade;
+        //     const estado = answer.uf;
+        //     const address = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        //     $('#address').val(address);
+
+        // })
+        // setTimeout(()=>{
+        //     $(this).find('span').addClass('d-none');
+        //     $(this).find('i').removeClass('d-none');
+        // }, 1000)
     })
 })
