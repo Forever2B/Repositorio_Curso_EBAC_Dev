99853c3568ac39a060fd54509b79259239fd19ee
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-03-19 13:51:51 -0300

    .
    ..

--- src/exercicio_ts.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const multiplicar = (var1, var2) => var1 * var2;
-const saudar = (name) => 'Olá ' + name;
-console.log(multiplicar(10, 5));
-console.log(saudar('Bruno'));
--- src/exercicio_ts.ts
+++
@@ -1,5 +0,0 @@
-const multiplicar = (var1:number , var2:number):number => var1 * var2;
-const saudar = (name:string):string => 'Olá ' + name;
-
-console.log(multiplicar(10,5));
-console.log(saudar('Bruno'));
