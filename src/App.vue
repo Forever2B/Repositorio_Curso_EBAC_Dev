fdfb3c837ff467e7dd7c327744651e1b62d07ae2
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-03-20 11:30:57 -0300

    .
    .

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,92 @@
 <script setup>
-  import {reactive} from 'vue';
-  import VHeader from './components/VHeader.vue';
-  import VForm from './components/VForm.vue';
-  import VList from './components/VList.vue';
-  // ----------- VARIÁVEIS REATIVAS --------------
-  const react = reactive({
-  
-    filtro: 'Todas',
-    tarefaTemp: '',
+import { reactive, watch } from 'vue';
 
-    tarefas: [
-      {
-        titulo: 'Estudar ES6',
-        finalizada: false,
-      },
-      {
-        titulo: 'Estudar Tailwind',
-        finalizada: false,
-      },
-      {
-        titulo: 'ir pra academia',
-        finalizada: false,
-      }
-    ]
-  })
-  // ---------------------------------------------
+// ----------- VARIÁVEIS REATIVAS --------------
+const react = reactive({
+  numero1: 0,
+  numero2: 0,
+  resultado: 0,
+  operation: 'add',
+});
+// ---------------------------------------------
 
-  const getTarefasPendentes = () => {return react.tarefas.filter(tarefa => !tarefa.finalizada)}
-  const getTarefasFinalizadas = () => {return react.tarefas.filter(tarefa => tarefa.finalizada)}
-  const cadastraTarefa = () => {
-    const tarefaNova = {
-      titulo: react.tarefaTemp,
-      finalizada: false,
-    }
-    react.tarefas.push(tarefaNova);
-    react.tarefaTemp = '';
-  };
+const calcularResultado = () => {
+  const { numero1, numero2, operation } = react;
+  switch (operation) {
+    case 'add':
+      react.resultado = numero1 + numero2;
+      break;
+    case 'sub':
+      react.resultado = numero1 - numero2;
+      break;
+    case 'mult':
+      react.resultado = numero1 * numero2;
+      break;
+    case 'div':
+      react.resultado = numero2 !== 0 ? numero1 / numero2 : 'Erro: Divisão por zero';
+      break;
+    default:
+      react.resultado = 0;
+  }
+};
 
-  const getTarefasFiltradas = () => {
-    const {filtro} = react;
-
-    switch (filtro){
-      case 'Pendentes':
-        return getTarefasPendentes();
-      case 'Finalizadas':
-        return getTarefasFinalizadas();
-      default:
-        return react.tarefas;
-    }};
-
-  console.log(react.tarefas)
-  // ---------------------------------------------
+watch(
+  () => [react.numero1, react.numero2, react.operation],
+  () => {
+    calcularResultado();
+  }
+);
 </script>
 
 <template>
-  <div class="container">
-
-    <VHeader :tarefas-pendentes="getTarefasPendentes().length"/>
-    <VForm :trocar-filtro="evento => react.filtro = evento.target.value" :tarefa-temp="react.tarefaTemp" :edita-tarefa-temp="evento => react.tarefaTemp = evento.target.value" :cadastra-tarefa="cadastraTarefa"/>
-    <VList :tarefas="getTarefasFiltradas()" :tarefas-pendentes="getTarefasPendentes().length"/>
-
+  <div class="color min-vh-100 m-0 pt-5">
+    <div class="container">
+      <h1 class="text-center mt-0 mb-5">Calculadora Aritimética</h1>
+      <form class="color2 p-4 rounded">
+        <div class="row mb-2">
+          <div class="col d-flex justify-content-center">
+            <input class="text-center fs-5" type="number" v-model.number="react.numero1" placeholder="Número 01" />
+          </div>
+        </div>
+        <div class="row mb-3 mt-3">
+          <div class="col d-flex justify-content-center">
+            <select class="text-center fs-5" v-model="react.operation"
+              @change="console.log(`Changed to: ${react.operation}`)">
+              <option value="add">Adição</option>
+              <option value="sub">Subtração</option>
+              <option value="mult">Multiplicação</option>
+              <option value="div">Divisão</option>
+            </select>
+          </div>
+        </div>
+        <div class="row mb-4">
+          <div class="col d-flex justify-content-center">
+            <input class="text-center fs-5" type="number" v-model.number="react.numero2" placeholder="Número 02" />
+          </div>
+        </div>
+        <div class="row">
+          <span class="text-center fs-3"><b class="text-white">O Resultado é:</b></span>
+          <span class="text-center fs-4 text-decoration-underline">
+            <b class="text-info">{{ react.resultado }}</b>
+          </span>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.color {
+  background-color: #b2bec3;
+}
+
+.color2 {
+  background-color: #636e72;
+}
+
+input[type='number']::-webkit-outer-spin-button,
+input[type='number']::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
 </style>
