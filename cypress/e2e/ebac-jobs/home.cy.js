d9845aedb30866ef7840719ff13353fd77477a1c
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-04-03 19:19:31 -0300

    .
    .

--- cypress/e2e/ebac-jobs/home.cy.js
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -10,21 +10,21 @@ describe ('Testes do componente Principal', () => {
         cy.get('input[type="tel"]').type('11112222')
         cy.get('button[type="submit"]').click()
 
-        cy.get('.sc-iAEyYk > :nth-child(4)').should('exist')
+        cy.get('h2').should('contain.text', "4 contatos na agenda")
     })
 
     it('Alterar um contato existente', () => {
-        cy.get('.sc-iAEyYk:nth-child(4) > .sc-gueYoa > .edit').click()
+        cy.get(':nth-child(5) > .sc-gueYoa > .edit').click()
 
         cy.get('input[type="text"]').clear().type('Contato EDITADO!!')
         cy.get('input[type="email"]').clear().type('ContatoEDITADO@testegmail.com')
         cy.get('input[type="tel"]').clear().type('4444')
         cy.get('button[type="submit"]').click()
 
-        cy.get('.sc-iAEyYk:nth-child(4) > .sc-gueYoa').should('contain', 'Contato EDITADO!!')
+        cy.get(':nth-child(5) > .sc-dmqHEX > .sc-eDDNvR > :nth-child(1)').should('contain.text', 'Contato EDITADO!!')
     })
     it('Deletar um contato da lista', () => {
-        cy.get('.sc-iAEyYk:nth-child(4) > .sc-gueYoa > .delete').should('exist').click().should('not.exist')
+        cy.get(':nth-child(5) > .sc-gueYoa > .delete').should('exist').click().should('not.exist')
     })
 
 }) 
