e15701d010a631ed6bf8ee69eef9f53e99c3ebe7
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-04-03 18:45:18 -0300

    .
    .

--- cypress/e2e/ebac-jobs/home.cy.js
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -1,9 +1,30 @@
 /// <reference types="cypress" />
 
-describe ('Testes do componente Home', () => {
-    it('Deve renderizar 4 vagas', () => {
-        cy.visit('https://ebac-jobs-e2e.vercel.app')
-        cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 4)
+describe ('Testes do componente Principal', () => {
+    beforeEach(() => {cy.visit('https://agenda-contatos-react.vercel.app')})
+
+    it('Inclusão de novo contato a lista', () => {
+
+        cy.get('input[type="text"]').type('Contato De Teste')
+        cy.get('input[type="email"]').type('ContatoDeTeste@testegmail.com')
+        cy.get('input[type="tel"]').type('11112222')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.sc-iAEyYk > :nth-child(4)').should('exist')
+    })
+
+    it('Alterar um contato existente', () => {
+        cy.get('.sc-iAEyYk:nth-child(4) > .sc-gueYoa > .edit').click()
+
+        cy.get('input[type="text"]').clear().type('Contato EDITADO!!')
+        cy.get('input[type="email"]').clear().type('ContatoEDITADO@testegmail.com')
+        cy.get('input[type="tel"]').clear().type('4444')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.sc-iAEyYk:nth-child(4) > .sc-gueYoa').should('contain', 'Contato EDITADO!!')
+    })
+    it('Deletar um contato da lista', () => {
+        cy.get('.sc-iAEyYk:nth-child(4) > .sc-gueYoa > .delete').should('exist').click().should('not.exist')
     })
 
 }) 
