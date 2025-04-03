61645a95d2da62daf7b341f1a08a651770a8eb14
Forever2B <130312444+Forever2B@users.noreply.github.com>
2025-04-03 16:01:10 -0300

    .
    .

---
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe ('Testes do componente Home', () => {
+    it('Deve renderizar 4 vagas', () => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app')
+        cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 4)
+    })
+
+}) 
